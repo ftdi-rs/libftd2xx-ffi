2a35cee1c7bbb9a22cb493b607b38a7c96517313
Alex M <7845120+newAM@users.noreply.github.com>
2020-06-14 12:43:54 -0700

    FTD2xx drivers building and running on Linux.

---
+++ README.md
@@ -0,0 +1,6 @@
+# libftd2xx-ffi
+
+Rust FFI bindings to the FTDI d2xx drivers.
+
+This is still a work-in-progress, building for linux is functional, building
+for Windows is not yet working.
---
+++ examples/num_devices.rs
@@ -0,0 +1,18 @@
+extern crate libftd2xx_ffi;
+
+use libftd2xx_ffi::{FT_ListDevices, DWORD, FT_LIST_NUMBER_ONLY, FT_STATUS, PVOID};
+
+fn main() {
+    let mut num_devs: DWORD = 5;
+    let num_devs_ptr: *mut DWORD = &mut num_devs;
+    let dummy: PVOID = std::ptr::null_mut();
+    let status: FT_STATUS = unsafe {
+        FT_ListDevices(
+            num_devs_ptr as *mut std::ffi::c_void,
+            dummy,
+            FT_LIST_NUMBER_ONLY,
+        )
+    };
+    println!("Status: {}", status);
+    println!("Number of devices: {}", num_devs);
+}
