ba938354dbd9da39f32e110f9302aba5321d9b05
Alex M <alexmgit@protonmail.com>
2020-07-01 10:41:32 -0700

    Add pre-generated bindings.

---
+++ README.tpl
@@ -0,0 +1,7 @@
+{{badges}}
+[![crates.io](https://img.shields.io/crates/v/libftd2xx-ffi.svg)](https://crates.io/crates/libftd2xx-ffi)
+[![docs.rs](https://docs.rs/libftd2xx-ffi/badge.svg)](https://docs.rs/libftd2xx-ffi/)
+
+# {{crate}}
+
+{{readme}}
--- build.rs
+++ build.rs
@@ -1,29 +1,37 @@
 #[deny(unsafe_code)]
 use std::env;
-use std::path::PathBuf;
 
-#[cfg(all(target_os = "linux", target_arch = "x86_64"))]
-mod paths {
-    pub const HEADER: &str = "libftd2xx_src/linux/x64/ftd2xx.h";
-    pub const SEARCH: &str = "libftd2xx_src/linux/x64/build";
-}
+#[cfg(feature = "bindgen")]
+use bindgen;
 
-#[cfg(all(target_os = "linux", target_arch = "x86"))]
-mod paths {
-    pub const HEADER: &str = "libftd2xx_src/linux/x86/ftd2xx.h";
-    pub const SEARCH: &str = "libftd2xx_src/linux/x86/build";
-}
-
-#[cfg(all(target_os = "windows", target_arch = "x86_64"))]
-mod paths {
-    pub const HEADER: &str = "libftd2xx_src/windows/ftd2xx.h";
-    pub const SEARCH: &str = "libftd2xx_src/windows/amd64";
-}
-
-#[cfg(all(target_os = "windows", target_arch = "x86"))]
-mod paths {
-    pub const HEADER: &str = "libftd2xx_src/windows/ftd2xx.h";
-    pub const SEARCH: &str = "libftd2xx_src/windows/i386";
+cfg_if::cfg_if! {
+    if #[cfg(all(target_os = "linux", target_arch = "x86_64"))] {
+        mod paths {
+            pub const HEADER: &str = "libftd2xx_src/linux/x64/ftd2xx.h";
+            pub const SEARCH: &str = "libftd2xx_src/linux/x64/build";
+        }
+    } else if #[cfg(all(target_os = "linux", target_arch = "x86"))] {
+        mod paths {
+            pub const HEADER: &str = "libftd2xx_src/linux/x86/ftd2xx.h";
+            pub const SEARCH: &str = "libftd2xx_src/linux/x86/build";
+        }
+    } else if #[cfg(all(target_os = "windows", target_arch = "x86_64"))] {
+        mod paths {
+            pub const HEADER: &str = "libftd2xx_src/windows/ftd2xx.h";
+            pub const SEARCH: &str = "libftd2xx_src/windows/amd64";
+        }
+    } else if #[cfg(all(target_os = "windows", target_arch = "x86"))] {
+        mod paths {
+            pub const HEADER: &str = "libftd2xx_src/windows/ftd2xx.h";
+            pub const SEARCH: &str = "libftd2xx_src/windows/i386";
+        }
+    } else {
+        std::compile_error!(
+            "Unsupported target, please open an issue. \
+            target_os = {}, target_arch = {}",
+            target_os, target_arch
+        );
+    }
 }
 
 fn main() {
@@ -37,18 +45,22 @@ fn main() {
     println!("cargo:rustc-link-lib=ftd2xx");
     println!("cargo:rerun-if-changed={}", header.to_str().unwrap());
 
-    let bindings = bindgen::Builder::default()
-        .header(header.to_str().unwrap())
-        .whitelist_function("FT_.*")
-        .whitelist_type("FT_.*")
-        .whitelist_var("FT_.*")
-        .rustfmt_bindings(true)
-        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
-        .generate()
-        .expect("Unable to generate bindings");
+    cfg_if::cfg_if! {
+        if #[cfg(feature = "bindgen")] {
+        let bindings = bindgen::Builder::default()
+            .header(header.to_str().unwrap())
+            .whitelist_function("FT_.*")
+            .whitelist_type("FT_.*")
+            .whitelist_var("FT_.*")
+            .rustfmt_bindings(true)
+            .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+            .generate()
+            .expect("Unable to generate bindings");
 
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-    bindings
-        .write_to_file(out_path.join("bindings.rs"))
-        .expect("Couldn't write bindings!");
+        let out_path = std::path::PathBuf::from(env::var("OUT_DIR").unwrap());
+        bindings
+            .write_to_file(out_path.join("bindings.rs"))
+            .expect("Couldn't write bindings!");
+        }
+    }
 }
